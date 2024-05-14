82b988bea3dea8c4daae340ae7ed40f70b19aafd
kal23002375 <kal23002375@SA224-09>
2024-05-14 09:54:21 +0100

    Add project files.

---
+++ Attempt2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Attempt2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LblScore = new Label();
+            PbxTree = new Panel();
+            PbxLumberJack = new PictureBox();
+            PnlBranches = new Panel();
+            ((System.ComponentModel.ISupportInitialize)PbxLumberJack).BeginInit();
+            SuspendLayout();
+            // 
+            // LblScore
+            // 
+            LblScore.AutoSize = true;
+            LblScore.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            LblScore.Location = new Point(10, 8);
+            LblScore.Name = "LblScore";
+            LblScore.Size = new Size(37, 45);
+            LblScore.TabIndex = 0;
+            LblScore.Text = "0";
+            // 
+            // PbxTree
+            // 
+            PbxTree.BackColor = Color.FromArgb(128, 64, 0);
+            PbxTree.Location = new Point(300, 0);
+            PbxTree.Name = "PbxTree";
+            PbxTree.Size = new Size(100, 500);
+            PbxTree.TabIndex = 1;
+            // 
+            // PbxLumberJack
+            // 
+            PbxLumberJack.Image = Properties.Resources.Lumberjack;
+            PbxLumberJack.Location = new Point(185, 291);
+            PbxLumberJack.Name = "PbxLumberJack";
+            PbxLumberJack.Size = new Size(92, 123);
+            PbxLumberJack.SizeMode = PictureBoxSizeMode.StretchImage;
+            PbxLumberJack.TabIndex = 2;
+            PbxLumberJack.TabStop = false;
+            // 
+            // PnlBranches
+            // 
+            PnlBranches.BackColor = SystemColors.Control;
+            PnlBranches.Location = new Point(200, 0);
+            PnlBranches.Name = "PnlBranches";
+            PnlBranches.Size = new Size(300, 500);
+            PnlBranches.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 461);
+            Controls.Add(PbxLumberJack);
+            Controls.Add(PbxTree);
+            Controls.Add(LblScore);
+            Controls.Add(PnlBranches);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)PbxLumberJack).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label LblScore;
+        private Panel PbxTree;
+        private PictureBox PbxLumberJack;
+        private Panel PnlBranches;
+    }
+}
---
+++ Attempt2/Form1.cs
@@ -0,0 +1,103 @@
+//https://github.com/mooict/Side-Scrolling-Platform-game-in-Windows-Form/blob/main/Side%20Scrolling%20Game%20MOO%20ICT/Form1.Designer.cs
+
+
+namespace Attempt2
+{
+    public partial class Form1 : Form
+    {
+        int score;
+        bool goLeft;
+        bool goRight;
+        List<bool> treeMap = new List<bool>();
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            StartGame();
+        }
+        public void StartGame()
+        {
+            treeMap.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                if (random.Next(1, 3) == 1)
+                    treeMap.Add(true);
+                else
+                    treeMap.Add(false);
+            }
+            score = 0;
+            UpdateDisplay();
+        }
+        public void UpdateDisplay()
+        {
+            PnlBranches.Controls.Clear();
+            for (int i = 0; i < treeMap.Count; i++)
+            {
+                Panel p = new Panel();
+                p.BackColor = Color.Brown;
+                p.Size = new Size(100, 20);
+                if (treeMap[i])
+                {
+                    p.Location = new Point(0, 200 - i * 50);
+                }
+                else
+                {
+                    p.Location = new Point(200, 200 - i * 50);
+                }
+                PnlBranches.Controls.Add(p);
+                p.BringToFront();
+            }
+        }
+        public void MainEvent(object sender, EventArgs e)
+        {
+            if (goLeft && !treeMap[0])
+            {
+                score++;
+                treeMap.RemoveAt(0); 
+                if (random.Next(1, 3) == 1)
+                    treeMap.Add(true);
+                else
+                    treeMap.Add(false);
+                PbxLumberJack.Location = new Point(185, 291);
+            }
+            else if (goRight && treeMap[0])
+            {
+                score++;
+                treeMap.RemoveAt(0); 
+                if (random.Next(1, 3) == 1)
+                    treeMap.Add(true);
+                else
+                    treeMap.Add(false);
+                PbxLumberJack.Location = new Point(430, 291);
+            }
+            else
+            {
+                MessageBox.Show("You Lost!\nYour score was " + score);
+                StartGame();
+            }
+            goLeft = false;
+            goRight = false;
+            LblScore.Text = score.ToString();
+            UpdateDisplay();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+            MainEvent(sender, e);
+        }
+    }
+}
